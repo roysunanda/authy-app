20772a1b78dd5e086a34ff4f1cce8a819281585d
Sunanda Roy <roysunanda.update@gmail.com>
2026-09-21 23:48:52 +0530

    repo updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <LoginForm />
+    <div className='min-h-screen'>
+      <div className='overflow-hidden mx-auto w-full max-w-7xl'>
+        <h1>Welcome to Authy!</h1>
+        <p>This is a public page accessible to everyone.</p>
+      </div>
     </div>
   );
 }
